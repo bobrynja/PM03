77ffe4860d1f6736f35d6ceadc3e172b67d4dbea
stu-pksp120 <stu-pksp120@AMDVLA3511GW01>
2024-04-19 12:22:18 +0300

    Создание класса Person

---
+++ src/src/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace src
+{
+    class Person
+    {
+        public string last_name;
+        public string name;
+        public string age;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
